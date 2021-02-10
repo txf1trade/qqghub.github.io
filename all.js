901458eae73d5f6f66e2d8890c913edb3e68fca6
admingio <78607081+admingio@users.noreply.github.com>
2021-02-10 22:40:13 +0800

    Update all.js

--- all.js
+++ all.js
@@ -23,11 +23,11 @@ var datanls = function (key, value) {
     if (typeof value === 'string') {
         a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);                                             
         if (!a) {   
-            	if(+a[1]=='1899'&& +a[2]=='12'&&+a[3]=='30')
-		{
-                a[1]=ny; a[2]=nm; a[3]=nd;
-		return new Date(Date.UTC(+a[1], +a[2], +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
-		}    
+            //	if(+a[1]=='1899'&& +a[2]=='12'&&+a[3]=='30')
+		//{
+            //    a[1]=ny; a[2]=nm; a[3]=nd;
+		//return new Date(Date.UTC(+a[1], +a[2], +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
+	//	}    
         } else {
         return new Date(Date.UTC(+a[1], +a[2], +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
         }
