b3be0cb9882e1f547e5ca7089be2a79527063e00
admingio <78607081+admingio@users.noreply.github.com>
2021-02-11 02:55:01 +0800

    Update all.js

--- all.js
+++ all.js
@@ -17,20 +17,21 @@ Date.prototype.format = function(fmt)
 　　fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
 　　return fmt;
 }
-/*
+
 var datanls = function (key, value) {
     var a;
     if (typeof value === 'string') {
         a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
+	    window.alert(a);
         if (a) {
             return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
         }
     }
     return value;
 }; //true
-*/
 
 
+/*
 var nowd = new Date();
 var ny = nowd.getFullYear();	
 var nm = nowd.getMonth();
@@ -51,7 +52,7 @@ var datanls = function (key, value) {
     }
     return value;
 };
-
+*/
 
 function timezone(){
       var content = document.getElementById("UTCtime");
