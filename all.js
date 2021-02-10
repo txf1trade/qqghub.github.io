4f99a3c9a2bafb296052ddbdce0ef0999ee03e21
admingio <78607081+admingio@users.noreply.github.com>
2021-02-11 03:22:43 +0800

    Update all.js

--- all.js
+++ all.js
@@ -22,7 +22,6 @@ var datanls = function (key, value) {
     var a;
     if (typeof value === 'string') {
         a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
-	    window.alert(a);
         if (a) {
             return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
         }
