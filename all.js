db3be244e8fb4ce9daf1b581c1149ae248e4e549
admingio <78607081+admingio@users.noreply.github.com>
2021-02-10 18:48:26 +0800

    Update all.js

--- all.js
+++ all.js
@@ -20,10 +20,14 @@ Date.prototype.format = function(fmt)
 
 var datanls = function (key, value) {
     var a;
+    var hhmm = 
     if (typeof value === 'string') {
         a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
         if (a) {
-            return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6])).format("HH:mm");
+            var hm = new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
+            var hh=hm.getHours();
+　          var mm=hm.getMinutes();
+             return hh + ":" + mm
         }
     }
     return value;
