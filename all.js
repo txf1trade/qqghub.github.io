79f2450c9e6f30f1e9787a9082344e2bfc1b5925
admingio <78607081+admingio@users.noreply.github.com>
2021-02-11 23:44:15 +0800

    Update all.js

--- all.js
+++ all.js
@@ -1,53 +1,82 @@
 
+function result()
+{
+var name = document.getElementById("name");
+var xmlhttp;
+
+        if (window.XMLHttpRequest)
+          {// code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+          }
+        else
+          {// code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+          }
+        xmlhttp.onreadystatechange=function()
+          {
+			
+                  if (xmlhttp.readyState==4 && xmlhttp.status==200)      
+                  {
+					  var result=xmlhttp.responseText;
+                        //console.log(result);
+                        var obj = JSON.parse(result,datanls);//解析json字串為json物件形式                     
+                        var html = '<table border=1 width=100%>';//
+					    var temp;
+                        
+                        for (var i = 0; i < obj.length; i ++ ) {//
+                                html  += '<tr>';// 
+							
+                                for(j=0;j<obj[i].data.length;j++)
+                                { 
+                                    if(i==1 && j==3){
+                                        var temp = obj[i].data[j];
+                                        obj[i].data[j]=temp.substr(11,5);
+                                    }
+								  html+= '<td>'+obj[i].data[j]+'</td>';
+                                }
+                                html  += '</tr>';   
+                                console.log(JSON.stringify(html));         
+                        }
+                        html+="</table>";
+                        
+                        document.getElementById("name").innerHTML=html;
+                        if(obj.length==1) //只有一筆代表查不到資料
+                                console.log('查無資料');
+                  }
+
+          }
+    //
+    var url="https://script.google.com/macros/s/AKfycbwuOL7-0IeQ_8qhrouSy2lorJaCDwzZf3ynwqd6v5f2XuIqcEvPhbu6/exec"; 
+ // var url="https://script.google.com/macros/s/AKfycbzXhdAxYrzvvdodxhF14jm7r8h94bL8aJNkfNsYRKJIpSI0kXMZ_bee/exec";  行止表
+        xmlhttp.open("get",url+"?name="+encodeURIComponent(name.value),true);
+        xmlhttp.send();
+}
+
+var s;
+var now = new Date(); 
+var YY = now.getFullYear();
+var MM = now.getMonth();
+var DD = now.getDate();
+var hh = now.getHours();
+var mm = now.getMinutes();
 var datanls = function (key, value) {
     var a;
+    var i = 1;
     if (typeof value === 'string') {
-        a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
+        var re =/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/;
+		a = re.exec(value);
+		
         if (a) {
-            return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
-        }
+            var d = new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
+            // var d = new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("HH:mm");
+            return d;
+        } 
     }
+  //  console.log("*--*");
+    console.log(value);
     return value;
-}; //true
-
-
-/*
-//var nowd = new Date();
-//var YY = nowd.getFullYear();	
-//var MM = nowd.getMonth();
-//var DD = nowd.getDate();
-
-var datanls = function (key, value) {
-    var a;
-    if (typeof value === 'string') {
-        a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);                                             
-          if (RegExp(/1899-12-30/).test(value)) {    
-	      //  a[1]=ny; a[2]=nm; a[3]=nd;
-            window.alert("hello world");
-	    // return new Date(Date.UTC(+a[1], +a[2], +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
-	    // return a[4] + ":" + a[5];
-          } else {
-               return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
-            }
-       // 
-    }
-    return value;
-};
-*/
-
-/*
-var datanls = function (key, value) {
-    var a;
-    if (typeof value === 'string') {
-        a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
-        if (a) {
-            return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
-        }
-    }
-    return value;
-}; //true
-*/
-
+}; //true	
+	
 Date.prototype.format = function(fmt)
 {
 　　var o = {
@@ -67,68 +96,3 @@ Date.prototype.format = function(fmt)
 　　fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
 　　return fmt;
 }
-
-function timezone(){
-      var content = document.getElementById("UTCtime");
-      var options = { 
-      timeZone: "Asia/Taipei", 
-      year: 'numeric', month: 'numeric', day: 'numeric', 
-      hour: 'numeric', minute: 'numeric', second: 'numeric' 
-      };
-         var formatter = new Intl.DateTimeFormat([], options);
-         var localTime = formatter.format(new Date(content.value));
-         document.getElementById("tzok").textContent=localTime.toString();
-}   
-<!-- 取得現在時間 -->
-function gettime(){  
-     var d = new Date();
-     var h=d.getHours();
-　   var m=d.getMinutes();
-     document.getElementById("nowt").innerHTML = h +'時:' + m + '分'; 
-}
-
-function result()
-{
-var name = document.getElementById("name");
-var xmlhttp;
-
-        if (window.XMLHttpRequest)
-          {// code for IE7+, Firefox, Chrome, Opera, Safari
-          xmlhttp=new XMLHttpRequest();
-          }
-        else
-          {// code for IE6, IE5
-          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-          }
-        xmlhttp.onreadystatechange=function()
-          {
-                  if (xmlhttp.readyState==4 && xmlhttp.status==200)      
-                  {
-                        var result=xmlhttp.responseText;
-                        var obj = JSON.parse(result,datanls);//解析json字串為json物件形式
-                                                
-                        var html = '<table border=1 width=100%>';//
-                        
-                        for (var i = 0; i < obj.length; i ++ ) {//
-                                html  += '<tr>';//
-                                for(j=0;j<obj[i].data.length;j++)
-                                {                               
-                                  html+= '<td>'+obj[i].data[j]+'</td>'; 
-                                }
-                                html  += '</tr>';            
-                        }
-                        html+="</table>";
-                        
-                        document.getElementById("result").innerHTML=html;
-                        if(obj.length==1) //只有一筆代表查不到資料
-                                alert('查無資料');
-                  }
-
-          }
-    <!-- TEST -->
-    var url="https://script.google.com/macros/s/AKfycbwuOL7-0IeQ_8qhrouSy2lorJaCDwzZf3ynwqd6v5f2XuIqcEvPhbu6/exec"; 
- // var url="https://script.google.com/macros/s/AKfycbzXhdAxYrzvvdodxhF14jm7r8h94bL8aJNkfNsYRKJIpSI0kXMZ_bee/exec";  行止表
-        xmlhttp.open("get",url+"?name="+encodeURIComponent(name.value),true);
-        xmlhttp.send();
-}
-
