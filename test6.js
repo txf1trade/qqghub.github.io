37680ea8b911a37377c7b9b9b014f91b97e3e096
admingio <78607081+admingio@users.noreply.github.com>
2021-05-29 10:54:32 +0800

    Update test6.js

--- test6.js
+++ test6.js
@@ -21,7 +21,7 @@ var xmlhttp;
 
                         var obj = JSON.parse(result,datafmt);//解析json字串為json物件形式
                                 console.log(JSON.stringify(obj[1].data[3]));
-                        var html = '<table border=1 width=100%>';//
+                        var html = '<table border=1 width:inherit>';//
 			var temp;
                         for (var i = 0; i < obj.length; i ++ ) {//
                                 html  += '<tr>';//
