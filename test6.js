505727a68d24c1875a0845e0b593f36c7d2654d6
admingio <78607081+admingio@users.noreply.github.com>
2021-05-30 06:07:36 +0800

    Update test6.js

--- test6.js
+++ test6.js
@@ -1,3 +1,4 @@
+
 function result()
 {
 var name = document.getElementById("name");
@@ -13,40 +14,41 @@ var xmlhttp;
           }
         xmlhttp.onreadystatechange=function()
           {
-
-                  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			
+                  if (xmlhttp.readyState==4 && xmlhttp.status==200)      
                   {
-			var result=xmlhttp.responseText;
-			 console.log(result);
-
+			var result=xmlhttp.responseText;  
+			// console.log(result);  
+                        
                         var obj = JSON.parse(result,datafmt);//解析json字串為json物件形式
-                                console.log(JSON.stringify(obj));
+                               // console.log(JSON.stringify(obj[1].data[3]));              
                         var html = '<table border=1 width=100%>';//
 			var temp;
                         for (var i = 0; i < obj.length; i ++ ) {//
-                                html  += '<tr>';//
+                                html  += '<tr>';// 						
                                 for(j=0;j<obj[i].data.length;j++)
-                                {
+                                { 
+
 				     if(i!='0'){
 					 var temp = obj[i].data[j];
-                                         switch(j) {
-                                              case 11:
-                                              case 12:
-                                              case 21:
+                                         switch(j) { 
+                                              case 11: 
+                                              case 12: 
+                                              case 21: 
                                               case 22:
                                                   obj[i].data[j]=temp.substr(11,5);
-                                                  break;
+                                                  break; 
                                       }
                                      }
                                             html+= '<td>'+obj[i].data[j]+'</td>';
                                  /*  debug console.log("i:"+i+" j:"+j+"   >"+obj[i].data[j]);
-					console.log(html);
+					console.log(html); 
 				    */
                                 }
-                                html  += '</tr>';
+                                html  += '</tr>';            
                         }
                         html+="</table>";
-
+                        
                         document.getElementById("result").innerHTML=html;
                         if(obj.length==1) //只有一筆代表查不到資料
                                 console.log('查無資料');
@@ -54,7 +56,7 @@ var xmlhttp;
 
           }
     //
-    var url="https://script.google.com/macros/s/AKfycbymCh_mGrkvnkDWCU2-LyKxriBLal-ma3rAsUKsQHzuFky7xP7UmE5aj78h4oGxffzxxA/exec"; //即時行止表
+    var url="https://script.google.com/macros/s/AKfycbx_iGfvM_r83SkzhOyqVGS2a4NffeVkq6qW9jnwZ54YS0jdLo1LckaTHWcvT6fMga-8EQ/exec"; //即時行止表
         xmlhttp.open("get",url+"?name="+encodeURIComponent(name.value),true);
         xmlhttp.send();
 }
@@ -68,12 +70,12 @@ var datafmt = function (key, value) {
             console.log(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6]));
             return new Date(Date.UTC(+a[1], +a[2]-1, +a[3], +a[4], +a[5], +a[6])).format("yyyy-MM-dd HH:mm:ss");
         }
-
+        
     }
     //console.log(value);
     return value;
-}; //true
-
+}; //true	
+	
 Date.prototype.format = function(fmt)
 {
 　　var o = {
